7af7d4fe17c3504ccda6a1b82cb328a2e95d872d
David Lopez <lopezda314@gmail.com>
2019-06-15 12:41:03 -0400

    Pick a random problem to use every time.

--- src/App.tsx
+++ src/App.tsx
@@ -84,14 +84,14 @@ class Gameboard extends React.Component<GameboardProps, GameboardState> {
           <OperationButton operationSymbol={"÷"} />
         </div>
         <div className="topNumberSymbolRow">
-          <NumberButton numberSymbol={this.state.problems[0][0]}/>
+          <NumberButton numberSymbol={this.state.problems[this.state.problem][0]}/>
         </div>
         <div className="middleNumberSymbolRow">
-          <NumberButton numberSymbol={this.state.problems[0][1]}/>
-          <NumberButton numberSymbol={this.state.problems[0][2]}/>
+          <NumberButton numberSymbol={this.state.problems[this.state.problem][1]}/>
+          <NumberButton numberSymbol={this.state.problems[this.state.problem][2]}/>
         </div>
         <div className="bottomNumberSymbolRow">
-          <NumberButton numberSymbol={this.state.problems[0][3]}/>
+          <NumberButton numberSymbol={this.state.problems[this.state.problem][3]}/>
         </div>
       </div>
     );
